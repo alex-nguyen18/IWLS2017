24c838ef5a8492c54afa8adacba23ce27c258a34
Caitlin <slickerc@gmail.com>
2020-11-14 16:21:09 -0600

    some print to file statements

--- src/yig2verilog.cpp
+++ src/yig2verilog.cpp
@@ -21,9 +21,11 @@ int main(int argc, char *argv[]){
 		cout << "Usage: ./yig2verilog <input benchmark> <output file name>" << endl;
 		return 1;
 	}
-	cout << argv[1] << endl;
+	//cout << argv[1] << endl;
 	string str;
 	ifstream file_in(argv[1], ifstream::in);
+	ofstream outfile;
+	outfile.open(argv[2]);
 	int cnt = 0;
 	while(getline(file_in,str)){
 		//cout << str << endl;
@@ -36,13 +38,14 @@ int main(int argc, char *argv[]){
 		}
 		else {
 			switch(cnt){
-			case 0: cout << "This was the first module ;" << endl; break;
+			case 0: //cout << "This was the first module ;" << endl; break;
 			case 1: //input
 				//cout << str << " " << buflen << " " << cnt << endl;
 				for (int i = buflen-1; i>0; i--){
 					if(str[i] == 'i'){//find last entry and take index
 						num_inputs = std::atoi(str.substr(i+1,buflen-i-1).c_str())+1;
-						cout << i << " " << atoi(str.substr(i+1,buflen-i-1).c_str())+1 << endl;
+						//cout << i << " " << atoi(str.substr(i+1,buflen-i-1).c_str())+1 << endl;
+						outfile << ".i " << num_inputs << "\n";
 						break;
 					} 
 				}
@@ -53,7 +56,8 @@ int main(int argc, char *argv[]){
 					if(str[i] == 'o'){ //find last entry and take index
 						num_outputs = std::atoi(str.substr(i+1,buflen-i-1).c_str())+1;
 						output_list = new yig [num_outputs]; //generate space for outputs
-						cout << i << " " << atoi(str.substr(i+1,buflen-i-1).c_str())+1 << endl;
+						//cout << i << " " << atoi(str.substr(i+1,buflen-i-1).c_str())+1 << endl;
+                        outfile << ".o " << num_outputs << "\n";
 						break;
 					} 
 				}
@@ -64,14 +68,15 @@ int main(int argc, char *argv[]){
 					if(str[i] == 'n'){//find last entry and take index
 						num_wires = std::atoi(str.substr(i+1,buflen-i-1).c_str())-num_outputs-num_inputs; 
 						wire_list = new yig [num_wires]; //generate space for the YIGs to build in
-						cout << i << " " << atoi(str.substr(i+1,buflen-i-1).c_str())+1 << endl;
+						//cout << i << " " << atoi(str.substr(i+1,buflen-i-1).c_str())+1 << endl;
+                        outfile << ".w " << num_wires << "\n";
 					break;
 					}
 				}
 				break;
 			default: //aigs -> yigs
-				cout << str << " " << buflen << " " << cnt << endl;
-				char a1[5], a2[5], op[5], a3[5];
+				//cout << str << " " << buflen << " " << cnt << endl;
+				char a1[8], a2[8], op[8], a3[8];
 				int success = sscanf(str.c_str(),"%*s %s %*s %s %s %s",a1, a2, op, a3);
 				string A1(a1);
 				string A2(a2);
@@ -93,17 +98,23 @@ int main(int argc, char *argv[]){
 				} 
 				else if (success == 2){ // "assign A1 = A2;" Note A2 can be '1'bx'; //probably will never call this, but just in case
 					A2 = A2.substr(0,A2.size()-1);
-					int id = std::atoi(A1.substr(1,A1.size()-1).c_str()) + num_inputs; 
+					int id;
+                    if (A1[0]=='n')
+                        id = std::atoi(A1.substr(1,A1.size()-1).c_str()) - num_inputs - num_outputs - 1;
+                    else if (A1[0]=='p')
+                        id = std::atoi(A1.substr(2,A1.size()-1).c_str()) + num_inputs;
+					id = std::atoi(A1.substr(1,A1.size()-1).c_str()) + num_inputs; 
 					wire_list[id].size = 0;
 					parse_arg(&wire_list[id],A2,1);
 				}
 			}
 			cnt++;
-			cout << cnt << endl;	
+			//cout << cnt << endl;	
 		}
 		
 	}
-	cout << "num wires: " << num_wires << " num inputs " << num_inputs << " num outputs " << num_outputs << endl;
+	//cout << "num wires: " << num_wires << " num inputs " << num_inputs << " num outputs " << num_outputs << endl;
+	outfile.close();
 	delete output_list;
 	delete wire_list;	
 }

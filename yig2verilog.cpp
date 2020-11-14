7056605c5ef07ae10a91d7af9892943d9f226f78
Caitlin <slickerc@gmail.com>
2020-11-14 12:18:18 -0600

    Committing changes here so you can see the diff easily

--- yig2verilog.cpp
+++ yig2verilog.cpp
@@ -1,13 +1,11 @@
 #include <fstream>
 #include <string>
+#include <iostream>
 #include <stdio.h>
+#include <cstdlib>
+#include "yig.h"
 
-struct yig{
-	int inp[3]; //indices of inp or wires
-	bool pol[3]; // true if inverted
-	int size; // size of top YIG; if 0, just a wire, if 1, MIG. Build larger ones recursively
-	yig* y1, y2, y3; // pointers to constituent YIGs so we have access to their inputs
-}
+using namespace std;
 
 int num_inputs;
 int num_outputs;
@@ -18,15 +16,13 @@ yig* output_list;
 void parse_arg(yig* y,string a,int id); //update yig pass-by-ptr
 //some sort of DFS optimization per output?
 
-using namespace std;
-
 int main(int argc, char *argv[]){
-	if (argc!=2){
+	if (argc!=3){
 		cout << "Usage: ./yig2verilog <input benchmark> <output file name>" << endl;
 		return 1;
 	}
 	string str;
-	ifstream fine_in(argv[0];
+	ifstream file_in(argv[0]);
 	while(getline(file_in,str)){
 		int buflen = str.size();
 		int cnt = 0;
@@ -37,14 +33,14 @@ int main(int argc, char *argv[]){
 			case 1: //input
 				for (int i = buflen-1; i>0; i--){
 					if(str[i] == 'i'){//find last entry and take index
-						num_inputs = atoi(str.substr(i+1,buflen-i-1))+1;
+						num_inputs = std::atoi(str.substr(i+1,buflen-i-1).c_str())+1;
 						break;
 					} 
 				}
 			case 2: //output
 				for (int i = buflen-1; i>0; i--){
 					if(str[i] == 'o'){ //find last entry and take index
-						num_outputs = atoi(str.substr(i+1,buflen-i-1))+1;
+						num_outputs = std::atoi(str.substr(i+1,buflen-i-1).c_str())+1;
 						output_list = new yig [num_outputs]; //generate space for outputs
 						break;
 					} 
@@ -52,27 +48,32 @@ int main(int argc, char *argv[]){
 			case 3: //wire
 				for (int i = buflen-1; i>0; i--){
 					if(str[i] == 'n'){//find last entry and take index
-						num_wires = atoi(str.substr(i+1,buflen-i-1))-num_outputs-num_inputs; 
+						num_wires = std::atoi(str.substr(i+1,buflen-i-1).c_str())-num_outputs-num_inputs; 
 						wire_list = new yig [num_wires]; //generate space for the YIGs to build in
-					break}
+					break;
+					}
 				}
 			default: //aigs -> yigs
-				string a1, a2, op, a3;
-				int success = sscanf(str,"%*s %s %*s %s %s %s",a1, a2, op, a3);
-				if (success == 4){ // "assign a1 = a2 op a3;"
-					a3 = a3.substr(0,a3.size()-1); // remove semicolon on a3
-					int id = atoi(a1.substr(1,a1.size()-1)) - num_inputs - num_outputs - 1; 
+				char a1[5], a2[5], op[5], a3[5];
+				int success = scanf(str.c_str(),"%*s %s %*s %s %s %s",a1, a2, op, a3);
+				string A1(a1);
+				string A2(a2);
+				string OP(op);
+				string A3(a3);
+				if (success == 4){ // "assign A1 = A2 OP A3;"
+					A3 = A3.substr(0,A3.size()-1); // remove semicolon on A3
+					int id = std::atoi(A1.substr(1,A1.size()-1).c_str()) - num_inputs - num_outputs - 1; 
 					wire_list[id].size = 1;
-					parse_arg(&wire_list[id],a2,1);
-					parse_arg(&wire_list[id],a3,2);
-					wire_list[id].inp[3] = -1;
-					wire_list[id].pol[3] = (op == '|'); //if or, 3rd input is 1; if and, 3rd input is 0
+					parse_arg(&wire_list[id],A2,1);
+					parse_arg(&wire_list[id],A3,2);
+					wire_list[id].inp[2] = -1;
+					wire_list[id].pol[2] = (OP == "|"); //if or, 3rd input is 1; if and, 3rd input is 0
 				} 
-				else if (success == 2){ // "assign a1 = a2;" Note a2 can be '1'bx'; //probably will never call this, but just in case
-					a2 = a2.substr(0,a2.size()-1);
-					int id = atoi(a1.substr(1,a1.size()-1)) - num_inputs - num_outputs - 1; 
+				else if (success == 2){ // "assign A1 = A2;" Note A2 can be '1'bx'; //probably will never call this, but just in case
+					A2 = A2.substr(0,A2.size()-1);
+					int id = std::atoi(A1.substr(1,A1.size()-1).c_str()) - num_inputs - num_outputs - 1; 
 					wire_list[id].size = 0;
-					parse_arg(&wire_list[id],a2,1);
+					parse_arg(&wire_list[id],A2,1);
 				}
 			}
 		}
@@ -81,16 +82,16 @@ int main(int argc, char *argv[]){
 	
 }
 
-void parse_arg(yig y, string a, int id){
-	string s = a;
-	if (s[0] == '~') {
-		y.pol[id] = true;
-		s = a.substr(1,a.size()-1);
-	}
-	if (s[0] == 'n'){ //wire
-		y.inp[id] = atoi(s.substr(1,s.size()-1))
-	}
-	else{ //input
-		y.inp[id] = atoi(s.substr(2,s.size()-1))
-	}
+void parse_arg(yig *y, string a, int id){
+    string s = a;
+    if (s[0] == '~') {
+        y->pol[id] = true;
+        s = a.substr(1,a.size()-1);
+    }
+    if (s[0] == 'n'){ //wire
+        y->inp[id] = atoi(s.substr(1,s.size()-1).c_str());
+    }
+    else{ //input
+        y->inp[id] = std::atoi(s.substr(2,s.size()-1).c_str());
+    }
 }

0ca97289e5fbbcbf7b273c8108fee2f3a75cfbca
Alex Nguyen <alexander.nguyen@utexas.edu>
2020-11-14 13:38:02 -0600

    add print_n flag on list of yigs

--- src/yig.h
+++ src/yig.h
@@ -1,8 +1,9 @@
 struct yig {
-    int inp[3]; //indices of inp or wires
-    bool pol[3]; // true if inverted
-    int size; // size of top YIG; if 0, just a wire, if 1, MIG. Build larger ones recursively
-    yig* y[3]; // pointers to constituent YIGs so we have access to their inputs
+	short inp[3]; //indices of inp or wires
+	bool pol[3]; // true if inverted
+	short size; // size of top YIG; if 0, just a wire, if 1, MIG. Build larger ones recursively
+	bool print_n = false; //do not print if true
+	yig* y[3]; // pointers to constituent YIGs so we have access to their inputs
 };
 
 

c01e6dcd29724357ef8c54b9ef6e6cac9fc2183d
Caitlin <slickerc@gmail.com>
2020-11-15 15:54:01 -0600

    Start converting yigs back to verilog

---
+++ run_verilog2yig.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+FILES="./benchmarks/*"
+
+for f in $FILES
+do
+	file_sanspath=${f##*/}
+	file_sansext=${file_sanspath%.*}
+	echo "./src/verilog2yig $f ./output_yigs/$file_sansext.yig"
+	./src/verilog2yig $f ./output_yigs/$file_sansext.yig
+done

3c908006801de0a002e25de74b6953f51e880f02
Caitlin <slickerc@gmail.com>
2020-11-18 12:50:17 -0600

    Add Y gate count to run script

--- run_verilog2yig.sh
+++ run_verilog2yig.sh
@@ -8,4 +8,5 @@ do
 	file_sansext=${file_sanspath%.*}
 	echo "./src/verilog2yig $f ./output_yigs/$file_sansext.yig"
 	./src/verilog2yig $f ./output_yigs/$file_sansext.yig
+	echo "    Y Gates in $file_sansext: " `grep -c "Y" ./output_yigs/$file_sansext.yig`
 done

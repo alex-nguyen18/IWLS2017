c9de981c78b102ba65ff20a8f253a49bea8353f4
Caitlin <slickerc@gmail.com>
2020-12-02 12:33:39 -0800

    trivial test case

---
+++ benchmarks/test.v
@@ -0,0 +1,10 @@
+module test (
+	pi0, pi1, pi2, pi3
+	po0);
+  	input pi0, pi1, pi2, pi3;
+  	output po0;
+  	wire n1, n2;
+	assign n1 = pi0 & pi1;
+	assign n2 = pi2 & pi3;
+	assign po0 = n1 & n2;
+endmodule
